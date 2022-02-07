33cc7e0b2a8ec6bd98570040cd62a6500b1e884e
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-02-07 03:44:27 +0200

    add most functionality

--- script.js
+++ script.js
@@ -5,13 +5,31 @@ const questionContainer = document.querySelector(".game-questions");
 // Counter
 const counterContainer = document.querySelector(".counter-container");
 const counterElement = document.querySelector(".counter");
+
 let counter = 3;
 
 // Quiz
+const quizElement = document.getElementById('quiz');
 const quizContainer = document.querySelector(".quiz-container");
-const rightOrWrongContainer = document.querySelector('.right-wrong-container');
+const rightOrWrongContainer = document.querySelector(".right-wrong-container");
+
+const right = document.querySelector(".right");
+
+// result
+const resultContainer = document.querySelector('.result-container');
+const playAgainContainer = document.querySelector('.play-again-container');
+
+const resultScore = document.querySelector('.result-score');
+const scoreElement = document.querySelector('.result-score span');
+const resultTime = document.querySelector('.result-time');
+
+
+const playAgainButton = document.querySelector('.play-again');
 
 let startGame = false;
+let score = 0;
+let question;
+let start;
 
 
 function clearChecked() {
@@ -39,24 +57,27 @@ startRound.addEventListener("click", function () {
     if (counter === 0) {
       clearInterval(intervalId);
       counterElement.textContent = "GO!";
-      startGame = true;
 
+      setTimeout(() => {
       test();
-      counterContainer.classList.add('hide');
-      quizContainer.classList.remove('hide');
+      counterContainer.classList.add("hide");
+      quizElement.classList.remove("hide");
+
+
+      startRound.classList.add("hide");
+      rightOrWrongContainer.classList.remove("hide");
       
-      startRound.classList.add('hide');
-      rightOrWrongContainer.classList.remove('hide');
+      question = initQuiz();
+      start =  startTimer();
+      }, 1000)
 
-
-
+  
+ 
     }
   }, 1000);
 });
 
-
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function randomNumber() {
   return Math.floor(Math.random() * numbers.length);
@@ -68,34 +89,141 @@ const result = [];
 
 function answers() {
   for (let i = 0; i < numbers.length; i++) {
-      firstNumber = numbers[randomNumber()];
-      secondNumber = numbers[randomNumber()];
-      result.push({
-          firstNumber,
-          secondNumber,
-          answer: firstNumber * secondNumber
-      })
+    firstNumber = numbers[randomNumber()];
+    secondNumber = numbers[randomNumber()];
+    result.push({
+      firstNumber,
+      secondNumber,
+      answer: firstNumber * secondNumber,
+    });
   }
 }
 answers();
 
 const something = [];
-for(let item of result) {
-    something.push(item.answer);
+for (let item of result) {
+  something.push(item.answer);
 }
-console.log(something);
-
 
 function test() {
-    for(let i = 0; i < result.length; i++) {
-        const p = document.createElement("p");
-        p.className = "quiz";
-        p.textContent = `${result[i].firstNumber} x ${result[i].secondNumber} = ${something[randomNumber()]} `
-        quizContainer.appendChild(p);
-        
-    }
+  for (let i = 0; i < result.length; i++) {
+    const p = document.createElement("p");
+    p.className = "quiz-question";
+
+    p.textContent = `${result[i].firstNumber} x ${result[i].secondNumber} = ${
+      something[randomNumber()]
+    } `;
+    quizContainer.append(p);
+  }
 }
 
-// answers();
-// createQuestions();
 
+let current = 0;
+let questions = 0;
+
+function nextQuestion() {
+  quizContainer.children[current].classList.remove("current-question");
+  if (current < 10) {
+    current++;
+  }
+  question = initQuiz();
+}
+
+
+rightOrWrongContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON") {
+    const {firstNumber , secondNumber , result} = question;
+    console.log(`${firstNumber} x ${secondNumber} = ${result}`);
+    questions += 1;
+
+   
+    if(e.target.textContent === 'Wrong') {
+
+        if(firstNumber * secondNumber !== result) {
+    
+            score += 1;
+        } else {
+          // penalty += 0.5;
+        }
+    }
+    if(e.target.textContent === 'Right') {
+
+        if(+firstNumber * +secondNumber === +result) {
+
+            score += 1;
+    
+        }  else {
+          // penalty += 0.5;
+        }
+    }
+
+    if(questions === 10) {
+        renderResult();
+    } else if(questions !== 10) {
+      nextQuestion();
+    }
+
+  }
+
+});
+
+function initQuiz() {
+
+    while(current < 10) {
+      const currentQuiz = quizContainer.children[current];
+
+      currentQuiz.classList.add("current-question");
+      const firstNumber = currentQuiz.textContent.slice(0, 1);
+      const secondNumber = currentQuiz.textContent.slice(4, 5);
+      const result = currentQuiz.textContent.slice(8);
+  
+      return {firstNumber, secondNumber, result}
+    }
+   
+
+}
+
+function renderResult() {
+  quizElement.style.height = 0;
+    quizContainer.classList.add('hide');
+    rightOrWrongContainer.classList.add('hide');
+
+
+    resultContainer.classList.remove('hide');
+    playAgainContainer.classList.remove('hide');
+
+    let end = endTimer(start);
+    // resultScore.textContent = end.toFixed(2);
+    resultTime.textContent = end.toFixed(2);
+
+    scoreElement.textContent = score;
+
+}
+
+function startTimer() {
+  const startingTime = new Date().getTime();
+  return startingTime;
+
+}
+
+function endTimer(startTime) {
+  return (new Date().getTime() - startTime) / 1000;
+}
+
+
+
+playAgainButton.addEventListener('click', function() {
+    reset();
+
+});
+
+
+function reset() {
+  playAgainContainer.classList.add('hide');
+  resultContainer.classList.add('hide');
+  questionContainer.classList.remove("hide");
+  startRound.classList.remove("hide");
+
+  counter = 3;
+
+}
